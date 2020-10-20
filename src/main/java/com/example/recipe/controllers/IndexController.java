e9ef075640f05377316ad89b59d41e498c872c77
Fatima <fatima.trtak@abc-techgroup.com>
2020-10-20 12:27:49 +0200

    added methods to some repository interfaces.

--- src/main/java/com/example/recipe/controllers/IndexController.java
+++ src/main/java/com/example/recipe/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package com.example.recipe.controllers;
 
+import com.example.recipe.domain.Category;
+import com.example.recipe.domain.UnitOfMeasure;
+import com.example.recipe.repositories.CategoryRepository;
+import com.example.recipe.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping("/")
-    public String getIndexPage(){
-        System.out.println("156");
+    public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id id: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
